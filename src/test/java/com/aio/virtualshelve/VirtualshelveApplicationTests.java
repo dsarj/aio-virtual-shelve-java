83ad3acb31391f5fa1933eab10ff5f1c88887806
Davi <daviaraujo.das@gmail.com>
2022-12-12 22:32:52 -0300

    Initial Commit

---
+++ src/test/java/com/aio/virtualshelve/VirtualshelveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aio.virtualshelve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualshelveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
