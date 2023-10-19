48d5d36084953fc00d3de2c3bc4ee3eda171d84e
Davi <daviaraujo.das@gmail.com>
2023-10-19 18:44:35 -0300

    Changing request mapping to books

--- src/main/java/com/aio/virtualshelve/controller/BookController.java
+++ src/main/java/com/aio/virtualshelve/controller/BookController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/book")
+@RequestMapping("/books")
 public class BookController {
 
     public ResponseEntity<String> index() {
