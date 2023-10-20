5247055eac35b856921df7407197cf7cfcf85b21
Davi <daviaraujo.das@gmail.com>
2023-10-20 19:57:23 -0300

    Mapping entities

--- src/main/java/com/aio/virtualshelve/model/Author.java
+++ src/main/java/com/aio/virtualshelve/model/Author.java
@@ -5,14 +5,19 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
+@Entity(name = "TB_AUTHOR")
 public class Author implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "NAME")
     private String name;
 }
--- src/main/java/com/aio/virtualshelve/model/Book.java
+++ src/main/java/com/aio/virtualshelve/model/Book.java
@@ -5,18 +5,26 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
+@Entity(name = "TB_BOOK")
 public class Book implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "NAME")
     private String name;
+    @Column(name = "START_YEAR")
     private Long startYear;
+    @Column(name = "FINISH_YEAR")
     private Long finishYear;
+    @JoinColumn(name = "AUTHOR_ID", referencedColumnName = "ID")
     private Author author;
 
 
